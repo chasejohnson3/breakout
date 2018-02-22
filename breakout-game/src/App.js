3600f85ee7cd0f4174c880b335ad1c7225c898ff
Junyan Yang <yangwu123789@gmail.com>
2018-02-22 17:47:02 -0600

    made the reflect

--- breakout-game/src/App.js
+++ breakout-game/src/App.js
@@ -79,13 +79,13 @@ class BallObj extends Component{
 
   updateBallDir()
   {
-    if(this.state.posX > 400 || this.state.posX <0 ) {
+    if(this.state.posX > 370 || this.state.posX <0 ) {
       this.setState((prevState, props) => ({
         ballSpeedX: -1 * prevState.ballSpeedX,
         //posY: prevState.posY + prevState.ballSpeedY
       }));
     }
-    if(this.state.posY>500 || this.state.posY<0)
+    if(this.state.posY>470 || this.state.posY<0)
     {
       this.setState((prevState, props) => ({
         ballSpeedY: -1 * prevState.ballSpeedY,
